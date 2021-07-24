80d26a0cc9cecb3536f715580207c9aff00f459d
xiaohaione <248543099@qq.com>
2021-07-24 20:49:42 +0800

    第一次提交

---
+++ yami-shop-security/src/main/java/com/yami/shop/security/service/YamiUser.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018-2999 广州亚米信息科技有限公司 All rights reserved.
+ *
+ * https://www.gz-yami.com/
+ *
+ * 未经允许，不可做商业用途！
+ *
+ * 版权所有，侵权必究！
+ */
+
+package com.yami.shop.security.service;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * 用户详细信息
+ */
+@Getter
+public class YamiUser extends User {
+
+	/**
+	 * 用户ID
+	 */
+	@Setter
+	private String userId;
+
+	@Setter
+	private String bizUserId;
+
+	@Setter
+	private String pic;
+
+	@Setter
+	private String name;
+
+	@Setter
+	private boolean debugger;
+
+	@Setter
+	private String password;
+
+	public YamiUser(String userId, String password, boolean enabled) {
+		super(userId, password, enabled,true, true, true , Collections.emptyList());
+		this.userId = userId;
+		this.password = password;
+	}
+
+	public YamiUser(String userId, String bizUserId, Integer appId, boolean enabled) {
+		super(appId + StrUtil.COLON + bizUserId, "", enabled,true, true, true , Collections.emptyList());
+		this.userId = userId;
+		this.bizUserId = bizUserId;
+	}
+}
